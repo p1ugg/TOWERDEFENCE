a39b7a8c30fde7c10c937ee8fc7912dc3d52ac14
p1ugg <67374872+p1ugg@users.noreply.github.com>
2023-01-14 17:15:36 +0300

    fix hitbox, понедельник без пива - неделя на вынос

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('data/saulgoodman.jpg').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
-        self.hitbox = self.rect.inflate(0, -10)
+        self.hitbox = self.rect.inflate(0, 0)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
